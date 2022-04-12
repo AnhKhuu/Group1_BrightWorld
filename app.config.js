48dd353dbe84514b431a15a38da86ab188554008
lananhkn <anhkhuu.31191023029@st.ueh.edu.vn>
2022-04-12 21:19:02 +0700

    create data list

--- app.config.js
+++ app.config.js
@@ -14,6 +14,9 @@ angular.
         }).
         when('/About', {
           template: '<about></about>'
+        }).
+        when('/Product-category/:productId', {
+          template: '<product-detail></product-detail>'
         })
     }
   ]);
