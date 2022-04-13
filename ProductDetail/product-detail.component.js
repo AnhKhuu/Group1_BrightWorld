b04d5f312363ddd7efdd3f6e81d993972aa79fd6
lananhkn <anhkhuu.31191023029@st.ueh.edu.vn>
2022-04-13 18:58:38 +0700

    create dropdown-cart

--- ProductDetail/product-detail.component.js
+++ ProductDetail/product-detail.component.js
@@ -4,9 +4,15 @@ angular.
     templateUrl: 'ProductDetail/product-detail.template.html',
     controller: ['$http','$routeParams',function ProductDetailCtrl($http, $routeParams) {
       var self = this;
+
+      self.setImage = function setImage(imageUrl) {
+        self.mainImageUrl = imageUrl;
+      };
+
       $http.get('products/'+$routeParams.productId+'.json').then(function(response)
       {
         self.product = response.data;
+        self.setImage(self.product.images[0]);
       });
       setTimeout(function(){
         $('.product-detail-carousel').owlCarousel(
